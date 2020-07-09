beb9f681397841736c77a30664b55a2bacd2a039
Carlos Guerrero <cguerrerodev@gmail.com>
2020-07-09 13:28:06 -0400

    Addition of navigation bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import connect from 'react-redux';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+
+class NavBar extends Component {
+
+
+    getUserOptions = ()=>{
+        if (this.props.user) {
+            return(
+                <Nav className="mr-auto">
+                    <Nav.Link href="/Pomodoro">Pomodoro</Nav.Link>
+                    <Nav.Link href="/Manual">Manual</Nav.Link>
+                </Nav>
+            )
+        } 
+        
+        return null;
+    }
+
+    render(){
+
+        return (
+
+            <Navbar bg="primary" expand="lg" variant="dark" fixed="top">
+                <Navbar.Brand href="#home">Guerrero's Time Tracker</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+
+                {this.getUserOptions()}
+
+                </Navbar.Collapse>
+            </Navbar>
+
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {user : state.user.user}
+}
+
+export default connect(mapStateToProps)(NavBar);
