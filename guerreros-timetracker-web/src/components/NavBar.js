57a951bb38d9b13f962bf12d683ca361709282e6
Carlos Guerrero <cguerrerodev@gmail.com>
2020-07-21 18:33:27 -0500

    Error solution in library importation

--- guerreros-timetracker-web/src/components/NavBar.js
+++ guerreros-timetracker-web/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import connect from 'react-redux';
+import {connect} from 'react-redux';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
